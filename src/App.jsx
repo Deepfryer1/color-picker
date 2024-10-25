c8e65671762e1f701029c7095cd879b89c122d20
Ben Savage <ben.savage2@icloud.com>
2024-10-25 17:22:39 -0400

    added corrected functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 // Write your Color component here
-import { useState } from 'react'
-const Color = (props) => {
-  return <div className={props.colors}></div>;
-};
+import { useState } from "react";
+// import Picker from './Picker';
+import Color from './Color';
+
 const App = () => {
+
+  const [selectedColor, setSelectedColor] = useState('#ff0000');
+
   return (
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+        <h2>Selected Color:{selectedColor}</h2>
+        <div id="colors-list">
+        <Color color="orange" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        <Color color="black" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        <Color color="purple" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        </div>
+          
       </div>
-      <div id="colors-list">
-        <Color colors="yellow" />
-  <Color colors="red"/>
-  <Color colors="green"/></div>
+      
     </div>
   );
 };
 
 export default App;
+
+//https://color-picker-5a4l.onrender.com
---
+++ src/Color.jsx
@@ -0,0 +1,12 @@
+// import Picker from "./Picker";
+
+
+const Color = ( color, setSelectedColor ) => {
+    
+    console.log(color);
+    return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+
+export default Color;
+
+
